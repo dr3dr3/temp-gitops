4ddffaaf1c8591f8adac04acecc41385df473ee6
André Dreyer <git@andredreyer.com>
2023-11-17 07:27:49 +1000

    Testing workflow

--- .github/actions-scripts/repos-get.mjs
+++ .github/actions-scripts/repos-get.mjs
@@ -11,14 +11,14 @@ const octokit = getOctokit(process.env.GITHUB_TOKEN);
 
 main();
 
-const { data: results } = await octokit.rest.repos.get({
-    owner: proess.env.REPO_OWNER,
-    repo: process.env.REPO_NAME,
-    }).archived;
-
-console.log( results );
-
-return results;
+async function getRepo() {
+    const { data: results } = await octokit.rest.repos.get({
+        owner: proess.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
+        }).archived;
+    console.log( results );
+    return results;
+}
 
 async function main() {
     const result = await getRepo();
