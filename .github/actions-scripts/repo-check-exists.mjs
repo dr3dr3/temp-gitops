17dc2c6260c5073d92c1a0b6c88c4666a0b0923f
André Dreyer <git@andredreyer.com>
2023-11-17 09:17:04 +1000

    Testing workflow

--- .github/actions-scripts/repo-check-exists.mjs
+++ .github/actions-scripts/repo-check-exists.mjs
@@ -3,11 +3,11 @@
 import { getOctokit } from "@actions/github";
 import { setOutput } from "@actions/core";
 
-console.assert(process.env.GITHUB_TOKEN, "GITHUB_TOKEN not present");
+console.assert(process.env.GHA_TOKEN, "GHA_TOKEN not present");
 console.assert(process.env.REPO_OWNER, "REPO_OWNER not present");
 console.assert(process.env.REPO_NAME, "REPO_NAME not present");
 
-const octokit = getOctokit(process.env.GITHUB_TOKEN);
+const octokit = getOctokit(process.env.GHA_TOKEN);
 
 main();
 
