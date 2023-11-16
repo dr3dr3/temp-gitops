638a4c9b72cb03051b193ae8e0e105890ab873da
André Dreyer <git@andredreyer.com>
2023-11-17 09:13:01 +1000

    Testing workflow

---
+++ .github/actions-scripts/repo-check-exists.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { getOctokit } from "@actions/github";
+import { setOutput } from "@actions/core";
+
+console.assert(process.env.GITHUB_TOKEN, "GITHUB_TOKEN not present");
+console.assert(process.env.REPO_OWNER, "REPO_OWNER not present");
+console.assert(process.env.REPO_NAME, "REPO_NAME not present");
+
+const octokit = getOctokit(process.env.GITHUB_TOKEN);
+
+main();
+
+async function checkUsersRepos() {
+    const { data:repoList } = await octokit.rest.repos.listForUser({
+        username: process.env.REPO_OWNER,
+        type: 'owner'
+        });
+    const repoListFiltered = repoList.filter( repoName => repoName.name === process.env.REPO_NAME );
+    const repoExists = (repoListFiltered.length == 0) ? false : true
+    // console.log( repoListFiltered );
+    return repoExists;
+}
+
+async function main() {
+    const result = await checkUsersRepos();
+    setOutput("result", result);
+};
