9184f9d897f1c758dcf54f35a8033b940f263398
André Dreyer <git@andredreyer.com>
2023-11-18 19:53:50 +1000

    Testing workflows

---
+++ .github/actions-scripts/repo-create-variables.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import { getOctokit } from "@actions/github";
+import { setOutput } from "@actions/core";
+
+console.assert(process.env.GHA_TOKEN, "GHA_TOKEN not present");
+console.assert(process.env.REPO_OWNER, "REPO_OWNER not present");
+console.assert(process.env.REPO_NAME, "REPO_NAME not present");
+console.assert(process.env.REPO_VARS, "REPO_VARS not present");
+
+const octokit = getOctokit(process.env.GHA_TOKEN);
+
+main();
+
+async function createRepoVariables() {
+    const items = JSON.parse('[{"name":"SITE_URL","value":"https://ci-slides.andredreyer.com/"}]');
+    for (const { name, value } of items) {
+        const { status:varCreated } = await octokit.rest.actions.createRepoVariable({
+            owner: process.env.REPO_OWNER,
+            repo: process.env.REPO_NAME,
+            name: name,
+            value: value,
+        });
+        console.log( varCreated );
+    }
+    return true;
+}
+
+async function main() {
+    const result = await createRepoVariables();
+    setOutput("result", result);
+};
