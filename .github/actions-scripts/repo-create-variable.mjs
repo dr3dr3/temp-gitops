181b86e840559ff5fa2d41f7ae19286fd90f60e2
André Dreyer <git@andredreyer.com>
2023-11-19 15:24:39 +1000

    Testing workflows

--- .github/actions-scripts/repo-create-variable.mjs
+++ .github/actions-scripts/repo-create-variable.mjs
@@ -6,29 +6,25 @@ import { setOutput } from "@actions/core";
 console.assert(process.env.GHA_TOKEN, "GHA_TOKEN not present");
 console.assert(process.env.REPO_OWNER, "REPO_OWNER not present");
 console.assert(process.env.REPO_NAME, "REPO_NAME not present");
-console.assert(process.env.REPO_VARS, "REPO_VARS not present");
+console.assert(process.env.VAR_NAME, "VAR_NAME not present");
+console.assert(process.env.VAR_VAL, "VAR_VAL not present");
 
 const octokit = getOctokit(process.env.GHA_TOKEN);
 
 main();
 
 async function createRepoVariables() {
-    const items = JSON.parse(process.env.REPO_VARS);
-    console.log(items)
-    for (const { name, value } of items) {
-        const { status:varCreated } = await octokit.rest.actions.createRepoVariable({
-            owner: process.env.REPO_OWNER,
-            repo: process.env.REPO_NAME,
-            name: name,
-            value: value,
-        });
-        console.log( varCreated );
-    }
+    const { status:varCreated } = await octokit.rest.actions.createRepoVariable({
+        owner: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
+        name: process.env.VAR_NAME,
+        value: process.env.VAR_VAL,
+    });
+    console.log( varCreated );
     return true;
 }
 
 async function main() {
-    console.log(process.env.REPO_VARS)
     const result = await createRepoVariables();
     setOutput("result", result);
 };
